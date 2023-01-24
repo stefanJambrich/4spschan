dd7fa6fd70cb2e619408011083fe822c8ef23899
stefan.jambrich <nutela415@gmail.com>
2023-01-24 21:30:47 +0100

    Design changes on homepage

---
+++ src/components/board-list/BoardList.tsx
@@ -0,0 +1,15 @@
+const BoardList = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href="">/prasit</a> - Sítě a praxe by Zvíře</li>
+                <li><a href="">/pro</a> - Skoro vše o programování</li>
+                <li><a href="">/web</a> - Webovky co ja vim</li>
+                <li><a href="">/ostatni</a> - Useless předměty</li>
+                <li><a href="">/c</a> - Random</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default BoardList;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,13 @@
 import logo from '../../assets/spsmb_logo.png'
-import './main.scss'
+import BoardList from '../board-list/BoardList';
 
 const Home = () => {
     return (
         <div id="home-wrapper">
+            <h1>Vítej na message boardě pro prumkaře!</h1>
             <img src={logo} alt="spsmb logo" id='logo' />
             <h1>4spschan.org</h1>
+            <BoardList />
         </div>
     );
 }
