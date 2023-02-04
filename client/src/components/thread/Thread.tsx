2393edbde83eac9ad4f27694dd1da6f2d1668955
stefan.jambrich <nutela415@gmail.com>
2023-02-04 12:51:08 +0100

    Modal for creating replies

--- client/src/components/thread/Thread.tsx
+++ client/src/components/thread/Thread.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import BoardLayout from "../board-layout/BoardLayout";
 import URL from '../../constants/URL.json';
+import Modal from 'react-modal';
 
 interface Props {
     title: string,
@@ -20,6 +21,7 @@ const Thread: React.FC<Props> = (props) => {
     const { threadId } = useParams();
     const [thread, setThread] = useState<ThreadReply>();
     const [reply, setReply] = useState<ThreadReply>();
+    const [modalIsOpen, handleModal] = useState<boolean>(false);
 
     useEffect(() => {
         axios.get(`${URL.URL}:${URL.PORT}/${URL.API}/thread/${threadId}`).then((data) => {
@@ -28,16 +30,38 @@ const Thread: React.FC<Props> = (props) => {
         });
     }, [useLocation().pathname]);
 
+    const handleCreate = async () => {
+
+    }
+
     return (
         <BoardLayout title={props.title} board={props.board}>
+            <Modal isOpen={modalIsOpen} contentLabel="Project Modal" id='project-modal'
+                style={{
+                    content: {
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)'
+                    }
+                }}
+            >
+
+            </Modal>
             <div id="thread-wrapper">
                 <div id="thread">
                     <img src={`${URL.URL}:${URL.PORT}/${thread?.img}`} alt="thread-image" />
                     <div id="text">
-                        <h2>{props.title} <span>Anonymous</span> No.{thread?.number}</h2>
+                        <div id="title">
+                            <h2>{props.title} <span>Anonymous</span></h2>
+                            <h2 onClick={() => handleModal(!modalIsOpen)} id="number">No.{thread?.number}</h2>
+                        </div>
                         <h3>{thread?.title}</h3>
                         <p>{thread?.description}</p>
                     </div>
+                </div>
+                <div id="replies">
+                    
                 </div>
             </div>
         </BoardLayout>
